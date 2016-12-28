0a44578ae05a768220496b377b1865ee57764013
Hguimaraes <hguimaraes@poli.ufrj.br>
2016-12-27 23:28:28 -0200

    Modeling new vue components and starting the design

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,27 @@
 <template>
-	<h1> Hello, World! </h1>
+  <div>
+    <nav class="white">
+      <div class="nav-wrapper">
+        <a href="#" class="brand-logo center"></a>
+      </div>
+    </nav>
+    <div class="container">
+      <dashboard></dashboard>
+    </div>
+  </div>
 </template>
 
 <script>
-	
+import Dashboard from './Dashboard.vue'
+
+export default {
+  name: 'home',
+  components: {
+    Dashboard
+  }
+}
 </script>
 
 <style>
-	
+
 </style>
