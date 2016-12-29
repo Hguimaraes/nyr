d168d27a23c221a1070e0e4596cadd60e7fa9002
Hguimaraes <hguimaraes@poli.ufrj.br>
2016-12-28 22:54:31 -0200

    Adding categories to the cards

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,19 +1,90 @@
 <template>
-  <div class="section">
-    <div class="row">
-      <div class="col s12 m6">
-        <div class="card blue-grey darken-1">
-          <div class="card-content white-text">
-            <span class="card-title">Card Title</span>
-            <p>I am a very simple card. I am good at containing small bits of information.
-            I am convenient because I require little markup to use effectively.</p>
-          </div>
-          <div class="card-action">
-            <a href="#">This is a link</a>
-            <a href="#">This is a link</a>
-          </div>
-        </div>
+<div>
+  <div class="row">
+    <div class="card small col s12 m4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="imgs/books.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Books<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Books<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+
+    <div class="card small col s12 m4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="imgs/fitness.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Fitness<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Fitness<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+
+    <div class="card small col s12 m4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="imgs/arts.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Arts<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Arts<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
       </div>
     </div>
   </div>
+  <div class="row">
+    <div class="card small col s12 m4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="imgs/education.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Education<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Education<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+
+    <div class="card small col s12 m4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="imgs/experience.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Experience<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Experience<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+
+    <div class="card small col s12 m4">
+      <div class="card-image waves-effect waves-block waves-light">
+        <img class="activator" src="imgs/others.png">
+      </div>
+      <div class="card-content">
+        <span class="card-title activator grey-text text-darken-4">Others<i class="material-icons right">more_vert</i></span>
+        <p><a href="#">This is a link</a></p>
+      </div>
+      <div class="card-reveal">
+        <span class="card-title grey-text text-darken-4">Others<i class="material-icons right">close</i></span>
+        <p>Here is some more information about this product that is only revealed once clicked on.</p>
+      </div>
+    </div>
+  </div>
+</div>
 </template>
